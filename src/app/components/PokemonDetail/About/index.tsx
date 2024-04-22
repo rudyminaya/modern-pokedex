fe51f5e7cff999e62a198071b6c89e3eb312af45
Rudy Minaya <rminaya@infocasas.com.uy>
2024-04-22 07:10:38 -0500

    fix: eliminando comillas

--- src/app/components/PokemonDetail/About/index.tsx
+++ src/app/components/PokemonDetail/About/index.tsx
@@ -29,7 +29,7 @@ const About = (props: Props) => {
   return (
     <div className={styles.about}>
       <h3>About</h3>
-      <p>"{props.description ?? ""}"</p>
+      <p>{props.description?`"${props.description}"`: ""}</p>
       <div className={styles.about__size}>
         <div className={styles.about__size__item}>
           <p>Height</p>
